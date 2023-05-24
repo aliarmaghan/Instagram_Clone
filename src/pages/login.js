c7c5360ad296794ea38ccb748e00f8cb9d239519
aliarmaghan <aliarmaghan78@gmail.com>
2023-05-24 23:55:23 +0530

    Update in Log In

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,7 @@
-import { useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import FirebaseContext from '../context/firebase';
 import { useContext, useEffect ,useState} from "react";
+import * as ROUTES from '../constants/routes';
 
 export default function Login() {
     const history = useNavigate();
@@ -12,7 +13,18 @@ export default function Login() {
     const [error, setError] = useState('');
     const isInvalid = password === '' || emailAddress === '';
 
-    const handleLogin = () => {};
+    const handleLogin = async(event) => {
+        event.preventDefault();
+
+        try {
+            await firebase.auth().signInWithEmailAndPassword(emailAddress,password);
+            history.push(ROUTES.DASHBOARD);
+        } catch (error) {
+            setEmailAddress('');
+            setPassword('');
+            setError(error.message);
+        }
+    };
 
     useEffect(() => {
         document.title = 'Login-Instagram'
@@ -29,7 +41,7 @@ export default function Login() {
             
             <div className="flex flex-col w-2/5">
                 <div className="flex flex-col items-center bg-white p-4 border 
-                border-gray-primary mb-4">
+                border-gray-primary mb-4 rounded">
                     <h1 className="flex justify-center w-full">
                         <img src="/image/logo.png" alt="Intagram" className="mt-2 w-6/12 mb-4" />
                     </h1>
@@ -53,17 +65,28 @@ export default function Login() {
                         <button 
                         disabled={isInvalid} 
                         type="submit"
-                        className={`bg-blue-500 text-white w-full rounded h-8 font-bold
+                        className={`bg-blue-medium text-white w-full rounded h-8 font-bold
                         ${isInvalid && 'opacity-50'}`}
                         >
                             Log In
                         </button>
                     </form>
                 </div>
-                <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary">
-                    <p className="text-sm font-medium">Don't have an account?{` `}</p>
+                <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
+                    <p className="text-sm font-medium">Don't have an account?{` `}
+                    <Link to={"/signup" } className="font-bold text-blue-medium">
+                        Sign up
+                    </Link>
+                    </p>
                 </div>
             </div>
         </div>
     );
 }
+
+//TODO: add to tailwind Config
+//bg-blue-medium -> hex value
+//text-red-primary -> hex value
+//text-gray-base -> hex value
+//border-gray-primary -> hex value
+//text-blue-primary -> hex value
